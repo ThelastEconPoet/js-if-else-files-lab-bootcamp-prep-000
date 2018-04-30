2ffbc2f758ac3542b1be41c12c8731edeccb77d4
Aaron G Meininger <a.g.meininger@gmail.com>
2018-04-30 15:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
-if (name == "Susan") {
-  consol.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
+
 
 // Don't worry about this
 module.exports = { name, height
